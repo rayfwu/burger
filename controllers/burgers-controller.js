f5d82c9a02c1b68af4ef8ed0a334a8de560436f4
Raymond Fwu <rayfwu@gmail.com>
2019-03-31 16:58:32 -0500

    set up controller

--- controllers/burgers-controller.js
+++ controllers/burgers-controller.js
@@ -1,16 +1,31 @@
 const express = require('express');
 var router = express.Router();
-
-router.get() {
-
-};
-
-router.post() {
-
-};
-
-router.put() {
-
-};
-
 var burger = require('../models/burger.js');
+
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+        var hbsObject = {
+            burger: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/", function(req, res) {
+    burger.create(req.body.name, function(result) {
+        res.json({ id: result.insertId })
+    });
+});
+
+router.put("/", function(req, res) {
+    burger.update(req.body.burgerToUpdate, true, function(result) {
+        if (result.changedRows == 0) {
+            return res.status(404), end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+module.exports = router;
